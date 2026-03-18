376e69b11d99a06480e7cd2f0d9e4235be135aeb
revantharunachalam <revanth.sbioa@gmail.com>
2026-03-18 08:33:41 +0530

    [https://github.com/revantharunachalam/android-sensor-testing/issues/1]: Kotlin-NDK template setup

---
+++ app/src/main/java/com/example/sensor_testing/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.sensor_testing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.example.sensor_testing.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Example of a call to a native method
+        binding.sampleText.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'sensor_testing' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'sensor_testing' library on application startup.
+        init {
+            System.loadLibrary("sensor_testing")
+        }
+    }
+}
